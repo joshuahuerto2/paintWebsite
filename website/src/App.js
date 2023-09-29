cd2f715bd6a2f4871409627a9935a31dcef72d6f
Joshua Huerto <joshuahuerto2@gmail.com>
2023-09-29 02:44:59 -0400

    add router skeleton

--- website/src/App.js
+++ website/src/App.js
@@ -1,26 +1,135 @@
 import logo from './logo.svg';
 import './App.css';
-import AppBar from '@mui/material/AppBar';
+import { 
+  AppBar,
+  Box,
+  Toolbar,
+  Typography,
+  Container,
+  Button,
+  IconButton,
+  Menu,
+  MenuItem,
+} from '@mui/material';
+import React from 'react';
 
-function MyButton() {
-  return (
-    <button>
-      Button
-    </button>
-  );
-
-}
+const pages = ['About', 'Photo Gallery', 'Upcoming Events', 'Store'];
 
 function App() {
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
+  const [anchorElUser, setAnchorElUser] = React.useState(null);
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  }
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
   return (
     <div>
       <AppBar position="static">
-        <button>
-          Button
-        </button>
+        <Container maxWidth="xl">
+          <Toolbar disableGutters>
+            <Typography
+              variant="h5"
+              noWrap
+              component="a"
+              href='/home'
+              sx={{
+                mr: 2,
+                display: { xs: 'none', md: 'flex' },
+                fontFamily: 'monospace',
+                fontWeight: 700,
+                letterSpacing: '.3rem',
+                color: 'inherit',
+                textDecoration: 'none',
+              }}
+            >
+              LOGO
+            </Typography>
+            <Typography
+              variant="h6"
+              noWrap
+              component="a"
+              href='/about'
+              sx={{
+                mr: 2,
+                display: { xs: 'none', md: 'flex' },
+                fontFamily: 'monospace',
+                fontWeight: 700,
+                color: 'inherit',
+                textDecoration: 'none',
+              }}
+              >
+              About
+            </Typography>
+            <Typography
+              variant="h6"
+              noWrap
+              component="a"
+              href='/gallery'
+              sx={{
+                mr: 2,
+                display: { xs: 'none', md: 'flex' },
+                fontFamily: 'monospace',
+                fontWeight: 700,
+                color: 'inherit',
+                textDecoration: 'none',
+              }}
+            >
+              Gallery
+            </Typography>
+            <Typography
+              variant="h6"
+              noWrap
+              component="a"
+              href='/upcoming'
+              sx={{
+                mr: 2,
+                display: { xs: 'none', md: 'flex' },
+                fontFamily: 'monospace',
+                fontWeight: 700,
+                color: 'inherit',
+                textDecoration: 'none',
+              }}
+            >
+              Upcoming
+            </Typography>
+            <Typography
+              variant="h6"
+              noWrap
+              component="a"
+              href='/store'
+              sx={{
+                mr: 2,
+                display: { xs: 'none', md: 'flex' },
+                fontFamily: 'monospace',
+                fontWeight: 700,
+                color: 'inherit',
+                textDecoration: 'none',
+              }}
+            >
+              Store
+            </Typography>
+          </Toolbar>
+        </Container>
       </AppBar>
+      <Container>
+        <Button>
+          Hello
+        </Button>
+      </Container>
     </div>
+
   );
 }
-
 export default App;
